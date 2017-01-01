131b1756b61b8cd121b7077dd3a5588ec3424aa6
Jess Askey <jess@askey.org>
2017-01-01 11:03:51 -0700

    - Added missing library.png for button - Added missing BinaryObject class

---
+++ Containers/CommandBinaryObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluke900Link.Containers
+{
+    public class CommandBinaryObject
+    {
+        public byte[] Bytes { get; set; }
+        public DeviceLibraryConfigurationItem Item { get; set; }
+        public int Offset { get; set; }
+
+        public CommandBinaryObject(DeviceLibraryConfigurationItem item, int offset, byte[] bytes)
+        {
+            Item = item;
+            Offset = offset;
+            Bytes = bytes;
+        }
+    }
+}
